0b11a371b20158a4d2af76fea0842c505bfdfd80
persnalitesaude-coder <persnalite.saude@gmail.com>
2025-09-27 18:58:20 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 from flask import Flask, request
- 
+
 app = Flask(_name_)
- 
+
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
-if request.method == 'GET':
-         verify_token = "personalite123"
-         if request.args.get("hub.verify_token") == verify_token:
-             return request.args.get("hub.challenge")
-         return "Token inválido", 403
- 
-     if request.method == 'POST':
-         data = request.json
-         print("Mensagem recebida:", data)
-         return "OK", 200
- 
- if _name_ == "_main_":
+    if request.method == 'GET':
+        verify_token = "personalite123"
+        if request.args.get("hub.verify_token") == verify_token:
+            return request.args.get("hub.challenge")
+        return "Token inválido", 403
+
+    if request.method == 'POST':
+        data = request.json
+        print("Mensagem recebida:", data)
+        return "OK", 200
+
+if _name_ == "_main_":
+    app.run(host='0.0.0.0', port=5000)
