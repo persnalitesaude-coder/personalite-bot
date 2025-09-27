e3e9e5a176a7a60df5b410f48929be3b7d110b4d
persnalitesaude-coder <persnalite.saude@gmail.com>
2025-09-27 19:22:47 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -9,11 +9,7 @@ def webhook():
         if request.args.get("hub.verify_token") == verify_token:
             return request.args.get("hub.challenge")
         return "Token inválido", 403
-
     if request.method == 'POST':
         data = request.json
         print("Mensagem recebida:", data)
         return "OK", 200
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
